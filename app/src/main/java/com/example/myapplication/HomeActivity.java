d9f456990d9ddd46c967962b026798daff6e6677
Josielcosta138 <cjosiel2@gmail.com>
2020-05-03 23:22:27 -0300

    atualização Home Imagens 03/05/2020.

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,15 +1,28 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaCas;
 import android.media.session.MediaSessionManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +38,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,27 +48,38 @@ public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = HomeActivity.class.getSimpleName(); // pega informações dessa classe
     private TextView nome, email; //cria os objetos
-    private Button btn_logout;
+    private Button btn_logout, btn_editar_photo;
     String getId;
     SessionManager sessionManeger;
     private static String URL_READ = "http://192.168.137.1/applogin/ler_detalhe.php";
     private static String URL_EDIT = "http://192.168.137.1/applogin/editar_detalhe.php";
     private Menu action;
+    Bitmap bitmap;
+    ImageView imagem_perfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //link com a classe R
+        //link com a classe R D ARIQUIVO XML DESING COM ESSE ARQUIVO JAVA
         nome = findViewById(R.id.edt_nome_home);
         email = findViewById(R.id.edt_email_home);
         btn_logout = findViewById(R.id.btn_logout);
-
+        btn_editar_photo = findViewById(R.id.btn_editar_foto);
+        imagem_perfil = findViewById(R.id.imagem_perfil);
+        
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 sessionManeger.Logout();
+            }
+        });
+
+        btn_editar_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                escolherFoto();
             }
         });
 
@@ -118,7 +145,153 @@ public class HomeActivity extends AppCompatActivity {
 
     }//fim getUserDetail
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getUserDetail();
+        // onResume
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_acao, menu);
+
+        action = menu;
+        action.findItem(R.id.menu_salvar).setVisible(false);
+        return true;
+        // return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_editar:
+                nome.setFocusableInTouchMode(true);
+                email.setFocusableInTouchMode(true);
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(nome, InputMethodManager.SHOW_IMPLICIT);
+
+                action.findItem(R.id.menu_editar).setVisible(false);
+                action.findItem(R.id.menu_salvar).setVisible(true);
+
+                return  true;
+            case R.id.menu_salvar:
+
+                SaveEditDetail();
+
+                action.findItem(R.id.menu_editar).setVisible(true);
+                action.findItem(R.id.menu_salvar).setVisible(false);
+
+                nome.setFocusableInTouchMode(false);
+                email.setFocusableInTouchMode(false);
+                nome.setFocusable(false);
+                email.setFocusable(false);
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }//switch
+
+    }//onOptionsItemSelected
+
+    private void SaveEditDetail(){
+
+        final String nome = this.nome.getText().toString().trim(); /// TRIM RETIRA OS ESPAÇOS DA DIGITAÇAO DO USUÁRIO
+        final  String email = this.email.getText().toString().trim();
+        final String id = getId;
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Salvando...");
+        progressDialog.show();
+
+        //passar as informaçoes para o arquivo PHP e pedier uma resposta dele.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_EDIT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();//DESAPAREÇA MSG
+                        // new JSONObject(response) METODO CONSTRUTOR/ RESPONSE VAI RECEBER RESPSTA
+                        try {
+                            JSONObject jasonObject = new JSONObject(response);
+                            String sucess = jasonObject.getString("sucess");
+
+                            if (sucess.equals("1")) {
+                                Toast.makeText(HomeActivity.this, "Sucesso!", Toast.LENGTH_SHORT).show();
+                                sessionManeger.createSession(nome, email, id);
+                            }//if
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(HomeActivity.this, "ERRO AO SALVAR!" + e.toString(), Toast.LENGTH_SHORT).show();
+                        }//catch
+                    }//onResponse
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "ERRO AO SALVAR!" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }//OnerroResponseListener
+                }//o
+
+        )//FIM stringRequest
+        {//Parans ou parametros de requisaçao
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("nome", nome);
+                params.put("email", email);
+                params.put("id", id);
+                return params;
+            }
+        };//StringRequest
+
+        RequestQueue requestQueue = Volley.newRequestQueue( this);
+        requestQueue.add(stringRequest);
+
+    }//SavaeEdidtDatail
+
+    //escolher foto
+   private void escolherFoto(){
+       Intent intent = new Intent();
+       intent.setType("imagem/*");
+       intent.setAction(Intent.ACTION_GET_CONTENT);
+       startActivityForResult(Intent.createChooser(intent, "selecione a foto!"), 1);
+   }// escolher foto
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //if de verificao se o usuario selecionou a imagem e eu certo
+        if (requestCode == 1 && resultCode == RESULT_OK  && data != null && data.getData()
+         != null){
+            Uri filePath = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imagem_perfil.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }// fim do catch
+            UploadPicture(getId, getStringImage(bitmap));
+            
+        }//if
+    }//on ActivityResult
+
+    private Object getStringImage(Bitmap bitmap) {
+        ByteArrayOutputStream ByteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ByteArrayOutputStream);
+
+        byte[] imageByteArray = ByteArrayOutputStream.toByteArray();
+        String encodeImage = Base64.encodeToString(imageByteArray, Base64.DEFAULT);
+
+        return encodeImage;
+
+    }//getStringImage
+    UploadPicture(getId, getStringImage(bitmap));
 
 }// class
 
